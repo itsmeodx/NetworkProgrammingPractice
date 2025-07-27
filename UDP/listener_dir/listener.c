faddccc3803e1c6c3725bbe96790ac700152e5e8
itsmeodx <oussama15ouaadich@gmail.com>
2025-07-27 10:44:15 +0100

    Switches message output to printf for clarity
    Replaces low-level write calls with printf to improve message formatting
    and output consistency. Adjusts message handling loop to better display
    received data and ensures output is flushed promptly for user feedback.

--- UDP/listener_dir/listener.c
+++ UDP/listener_dir/listener.c
@@ -92,26 +92,23 @@ int main(int argc, char const *argv[])
 		inet_ntop(theirAddr.ss_family,
 				  getinaddr((struct sockaddr *)&theirAddr), theirIP, sizeof(theirIP));
 
-		write(STDOUT_FILENO, "listener: got a message from ", 29);
-		write(STDOUT_FILENO, theirIP, strlen(theirIP));
-		write(STDOUT_FILENO, ":\n", 2);
-		write(STDOUT_FILENO, buf, rc);
-		while (!memchr(buf, '\r', rc))
+		printf("listener: got a message from %s:\n\"%.*s", theirIP, rc, buf);
+		fflush(stdout);
+		while (!memchr(buf, '\r', rc) || rc != 1)
 		{
 			if ((rc = recvfrom(sockFd, buf, MAXDSIZE, 0,
 							   (struct sockaddr *)&theirAddr, &addrLen)) == -1)
 			{
-				// printf();
-				write(STDOUT_FILENO, "\n", 1);
+				printf("\"\n");
 				perror("listner: recvfrom()");
 				close(sockFd);
 				return (EXIT_FAILURE);
 			}
-			if (memchr(buf, '\r', rc))
+			if (memchr(buf, '\r', rc) && rc == 1)
 				break;
-			write(STDOUT_FILENO, buf, rc);
+			printf("%.*s", rc, buf);
 		}
-		write(STDOUT_FILENO, "\n", 1);
+		printf("\"\n");
 	}
 
 	close(sockFd);
